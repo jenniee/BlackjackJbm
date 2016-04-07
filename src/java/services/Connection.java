1b5ced71403c482ba6aa4bb9c8e9f0cb30ca8cf0
c0637089 <c0637089@SB-N208-41.lambton.on.ca>
2016-04-07 14:26:33 -0400

    Added connection.

--- src/java/services/Connection.java
+++ src/java/services/Connection.java
@@ -5,10 +5,30 @@
  */
 package services;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author c0647063
  */
 public class Connection {
-    
+
+    public static java.sql.Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(java.sql.Connection.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        String hostname = "localhost";
+        String port = "3306";
+        String dbname = "blackjack";
+        String username = "root";
+        String password = "";
+        String jdbc = String.format("jdbc:mysql://%s:%s/%s", hostname, port, dbname);
+        return DriverManager.getConnection(jdbc, username, password);
+    }
 }
---
+++ src/java/services/Hash.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import static com.sun.javafx.font.FontResource.SALT;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author c0637089
+ */
+public class Hash {
+
+    
+    public static String hash(String password){
+        try {
+            String salted = password + SALT;
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] hash = md.digest(salted.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(b & 0xff).toUpperCase();
+                if (hex.length() == 1){
+                    sb.append("0");
+                }
+                sb.append(hex);
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex){
+            Logger.getLogger(Hash.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
