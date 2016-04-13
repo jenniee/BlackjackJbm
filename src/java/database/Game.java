5983cd9d2b3acc926d758e16c7ffc0cd46bd5160
c0637089 <c0637089@N208-F15-010.lambton.on.ca>
2016-04-12 21:37:07 -0400

    Added the Game.java variables, Getters, Setters and returnJson method; fixed some json formatting

--- src/java/database/Game.java
+++ src/java/database/Game.java
@@ -5,6 +5,7 @@
  */
 package database;
 
+
 /**
  *
  * @author kepoly
@@ -17,4 +18,74 @@ public class Game {
      * GAME META may need to extend this class.
      *
      */
+    
+    public int game_id;
+    public int user_id;
+    public String start_time;
+    public String end_time;
+    public int number_decks;
+    public int user_starting_balance;
+    public int user_bet_amount;
+
+    public int getGame_id() {
+        return game_id;
+    }
+
+    public void setGame_id(int game_id) {
+        this.game_id = game_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(String start_time) {
+        this.start_time = start_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public int getNumber_decks() {
+        return number_decks;
+    }
+
+    public void setNumber_decks(int number_decks) {
+        this.number_decks = number_decks;
+    }
+
+    public int getUser_starting_balance() {
+        return user_starting_balance;
+    }
+
+    public void setUser_starting_balance(int user_starting_balance) {
+        this.user_starting_balance = user_starting_balance;
+    }
+
+    public int getUser_bet_amount() {
+        return user_bet_amount;
+    }
+
+    public void setUser_bet_amount(int user_bet_amount) {
+        this.user_bet_amount = user_bet_amount;
+    }
+    
+    public String returnGameJson(int g_id, int u_id, String start, String end, int decks, int starting_balance, int bet){
+        return "{ \"game_id\" : \"" + g_id + "\", \"user_id\" : \"" + u_id + "\", \"start_time\" : \"" + start +
+                "\", \"end_time\" : \"" + end + "\", \"number_decks\" : \"" + decks +
+                "\", \"user_starting_balance\" : \"" + starting_balance + "\", \"user_bet_amount\" ; \"" + bet + "\" }";
+    }
 }
