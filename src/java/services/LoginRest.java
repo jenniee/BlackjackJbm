09b33f890b9efdf1ab193f69ea0107a63326e0b1
kepoly <spadamatthew@gmail.com>
2016-04-14 14:54:52 -0400

    Fixed SQL

--- src/java/services/LoginRest.java
+++ src/java/services/LoginRest.java
@@ -10,13 +10,10 @@ import database.UserController;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonArray;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import static javax.ws.rs.client.Entity.json;
 import javax.ws.rs.core.Response;
 import org.json.JSONArray;
 
